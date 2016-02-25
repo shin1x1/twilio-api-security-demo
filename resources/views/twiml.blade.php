eccc7e4d675be9e46fb043dbb578604b2aee24d9
shin1x1 <shin1x1@gmail.com>
2016-02-26 00:49:50 +0900

    Improve xml view template

--- resources/views/twiml.blade.php
+++ resources/views/twiml.blade.php
@@ -1,4 +1,4 @@
-<?xml version="1.0" encoding="UTF-8"?>
+<?= '<?xml version="1.0" encoding="UTF-8"?>' . PHP_EOL ?>
 <Response>
     <Say voice="woman" language="ja-JP">{{ $message }}</Say>
 </Response>
